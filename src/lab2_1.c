142b04da5070307783c2deba250990f11fb926a4
Mohammad Anas <mohammad-anas.m@edu.rtu.lv>
2026-09-22 18:56:20 +0000

    Add Lab 2 control flow exercises

---
+++ src/lab2_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int sum_to_n(int n) {
+  int sum = 0;
+
+  for (int i = 1; i <= n; i++) {
+    sum += i;
+  }
+
+  return sum;
+}
+
+int main(void) {
+  int n;
+
+  printf("Enter n: ");
+  scanf("%d", &n);
+
+  if (n < 1) {
+    printf("Error: n must be at least 1.\n");
+  } else {
+    printf("Sum from 1 to %d is %d.\n", n, sum_to_n(n));
+  }
+
+  return 0;
+}
